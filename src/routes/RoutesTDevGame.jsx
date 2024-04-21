e112ceb3a671deec3daee3322b82975fce6c10cb
Sebastian Muñoz <juan.munoz.rojas@correounivalle.edu.co>
2024-04-20 21:00:59 -0500

    level 3 created

--- src/routes/RoutesTDevGame.jsx
+++ src/routes/RoutesTDevGame.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Level1 from "../pages/level1/Level1";
 import Level2 from "../pages/level2/Level2";
+import Level3 from "../pages/level3/Level3";
 import Login from "../pages/login/Login";
 
 export default function RoutesTDevGame() {
@@ -10,6 +11,7 @@ export default function RoutesTDevGame() {
                 <Route path="/" element={<Login />} />
                 <Route path="/level1" element={<Level1 />} />
                 <Route path="/level2" element={<Level2 />} />
+                <Route path="/level3" element={<Level3 />} />
             </Routes>
         </BrowserRouter>
     )
