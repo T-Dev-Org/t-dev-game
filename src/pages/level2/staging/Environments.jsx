ef43e8cda0f188b12cb785370e9eb5b6a23362ec
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2024-04-21 13:53:39 -0500

    updated Environments for a local hdr (smaller)

--- src/pages/level2/staging/Environments.jsx
+++ src/pages/level2/staging/Environments.jsx
@@ -1,9 +1,17 @@
 import { Environment } from "@react-three/drei";
 
 export default function Environments() {
+
+    // Importar usando el Cubemap
+    // const basePath = "/assets/textures/cubemaps/autumn_field_puresky/";
+    // const fileNames = ['nx', 'ny', 'nz', 'px', 'py', 'pz'];
+    // const extFile = '.png';
+    // const fileUrls = fileNames.map(fileName => `${basePath}${fileName}${extFile}`);
+
     return (
         <Environment
-            files={"https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/4k/autumn_field_puresky_4k.hdr"}
+            // files={fileUrls} // Importar usando Cubemap (No funciona bien)
+            files={"/assets/textures/hdris/antumn_field_puresky/1k.hdr"}
             preset={null}
             background={false}
             ground={{
