4728e37789cff0083248184417aa409b56ab1864
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2024-04-23 16:53:40 -0500

    TD-13 TD-17 positioning directional light with Leva

--- src/pages/level2/lights/Lights.jsx
+++ src/pages/level2/lights/Lights.jsx
@@ -1,52 +1,52 @@
 import { useHelper } from "@react-three/drei";
 import { useControls } from "leva";
-import { useMemo, useRef } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import { Color, Vector3 } from "three";
 import { DirectionalLightHelper } from "three/src/Three.js";
 
 const Lights = () => {
 
+    const ambientLightRef = useRef();
     const directionalLightRef = useRef();
     useHelper(directionalLightRef, DirectionalLightHelper);
 
-    const optionsDirectionalLight = useMemo(() => {
+    const optionsAmbientLight = useMemo(() => {
         return {
-            intensityDL: { value: 1.4, min: 0, max: 6, step: 0.1 },
-            positionDL: { value: [40, 20, 80] },
-            targetPosition: { value: [30, 10, -40] }
+            intensityAL: { value: 0.40, min: 0.00, max: 1.00, step: 0.01 }
         }
     })
 
-    const { intensityDL, positionDL, targetPosition } = useControls("DirectionalLight", optionsDirectionalLight);
+    const optionsDirectionalLight = useMemo(() => {
+        return {
+            castShadowDL: true,
+            intensityDL: { value: 1.4, min: 0, max: 6, step: 0.1 },
+            positionDL: { value: [40, 20, 80] },
+            targetPositionDL: { value: [30, 12, -40] }
+        }
+    })
 
-    // Calcular el vector de dirección hacia el objetivo
-    const direction = useMemo(() => {
-        const lightPosition = new Vector3().fromArray(positionDL);
-        const target = new Vector3().fromArray(targetPosition);
-        return target.sub(lightPosition).normalize();
-    }, [positionDL, targetPosition]);
+    const { intensityAL } = useControls("Ambient Light", optionsAmbientLight);
+    const { castShadowDL, intensityDL, positionDL, targetPositionDL } = useControls("directionalLight", optionsDirectionalLight);
+
+    useEffect(() => {
+        console.log("Light updated")
+        if (directionalLightRef.current && directionalLightRef.current.target) {
+            directionalLightRef.current.target.position.fromArray(targetPositionDL);
+        }
+    }, [targetPositionDL]);
 
     return <>
         <ambientLight
-            intensity={0.8}
+            ref={ambientLightRef}
+            intensity={intensityAL}
         />
         <directionalLight
             ref={directionalLightRef}
-            castShadow={true}
+            castShadow={castShadowDL}
             position={positionDL}
             color={new Color("#FFF700")}
             intensity={intensityDL}
-            shadow-mapSize={[2048, 2048]}
-            shadow-camera-far={100}
-            shadow-camera-left={-10}
-            shadow-camera-right={10}
-            shadow-camera-top={10}
-            shadow-camera-bottom={-100}
-            target={directionalLightRef.current?.target} // Usar condicional opcional
         />
-        {directionalLightRef.current && (
-            <primitive object={directionalLightRef.current.target} position={targetPosition} />
-        )}
     </>
 }
 export default Lights;
