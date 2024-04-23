61c2d771a1baee9b3662f37cbeab141e360446ba
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2024-04-23 15:57:48 -0500

    updating lights, purification and atmosphere

--- src/pages/level2/lights/Lights.jsx
+++ src/pages/level2/lights/Lights.jsx
@@ -1,22 +1,52 @@
-import { Color } from "three";
+import { useHelper } from "@react-three/drei";
+import { useControls } from "leva";
+import { useMemo, useRef } from "react";
+import { Color, Vector3 } from "three";
+import { DirectionalLightHelper } from "three/src/Three.js";
 
 const Lights = () => {
+
+    const directionalLightRef = useRef();
+    useHelper(directionalLightRef, DirectionalLightHelper);
+
+    const optionsDirectionalLight = useMemo(() => {
+        return {
+            intensityDL: { value: 1.4, min: 0, max: 6, step: 0.1 },
+            positionDL: { value: [40, 20, 80] },
+            targetPosition: { value: [30, 10, -40] }
+        }
+    })
+
+    const { intensityDL, positionDL, targetPosition } = useControls("DirectionalLight", optionsDirectionalLight);
+
+    // Calcular el vector de dirección hacia el objetivo
+    const direction = useMemo(() => {
+        const lightPosition = new Vector3().fromArray(positionDL);
+        const target = new Vector3().fromArray(targetPosition);
+        return target.sub(lightPosition).normalize();
+    }, [positionDL, targetPosition]);
+
     return <>
         <ambientLight
-            intensity={0.5}
+            intensity={0.8}
         />
         <directionalLight
+            ref={directionalLightRef}
             castShadow={true}
-            position={[2, 10, 0]}
+            position={positionDL}
             color={new Color("#FFF700")}
-            intensity={2}
-            shadow-mapSize = {[2048, 2048]}
-            shadow-camera-far = {100}
-            shadow-camera-left = {-10}
-            shadow-camera-right = {10}
-            shadow-camera-top = {10}
-            shadow-camera-bottom = {-100}
+            intensity={intensityDL}
+            shadow-mapSize={[2048, 2048]}
+            shadow-camera-far={100}
+            shadow-camera-left={-10}
+            shadow-camera-right={10}
+            shadow-camera-top={10}
+            shadow-camera-bottom={-100}
+            target={directionalLightRef.current?.target} // Usar condicional opcional
         />
+        {directionalLightRef.current && (
+            <primitive object={directionalLightRef.current.target} position={targetPosition} />
+        )}
     </>
 }
 export default Lights;
--- src/pages/level2/staging/Environments.jsx
+++ src/pages/level2/staging/Environments.jsx
@@ -2,16 +2,11 @@ import { Environment } from "@react-three/drei";
 
 export default function Environments() {
 
-    // Importar usando el Cubemap
-    // const basePath = "/assets/textures/cubemaps/autumn_field_puresky/";
-    // const fileNames = ['nx', 'ny', 'nz', 'px', 'py', 'pz'];
-    // const extFile = '.png';
-    // const fileUrls = fileNames.map(fileName => `${basePath}${fileName}${extFile}`);
-
     return (
         <Environment
-            // files={fileUrls} // Importar usando Cubemap (No funciona bien)
-            files={"/assets/textures/hdris/antumn_field_puresky/1k.hdr"}
+            // files={"/assets/textures/hdris/antumn_field_puresky/1k.hdr"} // Importar como HDR
+            path="/assets/textures/cubemaps/autumn_field_puresky/"
+            files={['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png']}
             preset={null}
             background={false}
             ground={{
