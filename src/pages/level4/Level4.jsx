4e2542d211c774ab5d0b4b90cbec84c99e17dc27
Yissy <yissyposso12@gmail.com>
2024-04-25 17:32:39 -0500

    modificaciones del nivel

--- src/pages/level4/Level4.jsx
+++ src/pages/level4/Level4.jsx
@@ -1,21 +1,37 @@
 import { Physics } from "@react-three/rapier";
 import { Canvas } from "@react-three/fiber";
-import WorldLevel4 from "./world/WorldLevel4";
 import Ecctrl, { EcctrlAnimation } from "ecctrl";
+import Avatar from "./characters/avatar/Avatar";
+import { KeyboardControls } from "@react-three/drei";
+import useMovements from "../../utils/key-movements";
+import WorldLevel4 from "./world/WorldLevel4";
 
 export default function Level4() {
+    const map = useMovements();
+
     return (
-        <Canvas>
+        <KeyboardControls map={map} >
+
+            <Canvas>
+                <ambientLight
+                    intensity={0.4}
+                />
+                <directionalLight
+                    intensity={0.6}
+                    position={[0, 12, -14]} />
                 <Physics>
-                    <WorldLevel4/>
+                    <WorldLevel4 />
                     <Ecctrl
-                camInitDis={-2}
-                camMaxDis={-2}
-                maxVelLimit={5}
-                jumpVel={4}
-                position={[0, 2, 0]}
-            ></Ecctrl>
+                        camInitDis={-2}
+                        camMaxDis={-2}
+                        maxVelLimit={3}
+                        jumpVel={3}
+                        position={[0, 1, 15]}
+                    >
+                        <Avatar />
+                    </Ecctrl>
                 </Physics>
-        </Canvas>
+            </Canvas>
+        </KeyboardControls>
     )
 }
