c272372baba0ef451af2512d8db3c0a1c87e3a65
Yissy <yissyposso12@gmail.com>
2024-04-26 16:42:41 -0500

    texturas del templo y controls

--- src/pages/level4/Level4.jsx
+++ src/pages/level4/Level4.jsx
@@ -4,12 +4,13 @@ import Ecctrl, { EcctrlAnimation } from "ecctrl";
 import Avatar from "./characters/avatar/Avatar";
 import { KeyboardControls, OrbitControls  } from "@react-three/drei";
 import useMovements from "../../utils/key-movements";
-import { WorldLevel4 } from "./world/Level4World";
 import { Perf } from "r3f-perf";
 import { Suspense } from "react";
 import WelcomeText from "./abstractions/WelcomeText";
 import Lights from "../level1/lights/Lights";
 import Environments from "./staging/Environments";
+import { WorldLevel4 } from "./world/WorldLevel4";
+import Controls from "./controls/Controls"
 
 
 export default function Level4() {
@@ -29,7 +30,7 @@ export default function Level4() {
                         camInitDis={-2}
                         camMaxDis={-2}
                         maxVelLimit={5}
-                        jumpVel={6}
+                        jumpVel={7}
                         position={[0, 9, 115]}
                     >
                         <Avatar />
@@ -37,6 +38,7 @@ export default function Level4() {
                 </Physics>
                 <WelcomeText position={[0,0,110]}/>
                 </Suspense>
+                <Controls/>
             </Canvas>
         </KeyboardControls>
     )
