5a129e320aec843370e06d209bbabc838b563c74
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2024-05-09 11:52:39 -0500

    added button interaction and stairs to level 1

--- src/pages/level1/Level1.jsx
+++ src/pages/level1/Level1.jsx
@@ -18,69 +18,68 @@ import Texts from "./abstractions/Texts";
 import GameUI from "../../utils/components/layouts/GameUI/GameUI";
 import NextLevelButton from "../../utils/components/layouts/GameUI/components/NextLevelButton";
 // import ZoneSensors from "./world/ZoneSensors";
-import Press from "../../globals/interactables/Button";
 import Rat from "../../globals/villains/Rat";
 import ZoneSensors from "./world/ZoneSensors";
 import { useLifeState } from "../../utils/components/controller/CharacterLife";
 import nullMovements from "../../utils/null-movements";
+import Interactables from "./interactables/Interactables";
 
 export default function Level1() {
-    const map = useMovements();
+  const map = useMovements();
 
-    const nullMap = nullMovements();
+  const nullMap = nullMovements();
 
-    const lifeState = useLifeState();
+  const lifeState = useLifeState();
 
-    // Estado local para controlar si se muestra la vida o no
-    const [displayLife, setDisplayLife] = useState(true);        
+  // Estado local para controlar si se muestra la vida o no
+  const [displayLife, setDisplayLife] = useState(true);
 
-    useEffect(() => {
-        console.log(`[Level1.jsx] Change on LifeValue, is ${lifeState.value} now`);
-  
-        // Cambios en mostrar/ocultar elementos dependiendo del valor
-        if (lifeState.value <= 0) {
-          setDisplayLife(false);
-          console.log("Mori");
-        } else {
-          setDisplayLife(true);
-        }
-      }, [lifeState.value]); // Depende unicamente de cambios en lifeState.value    
+  useEffect(() => {
+    console.log(`[Level1.jsx] Change on LifeValue, is ${lifeState.value} now`);
 
-    return (
-        <KeyboardControls map={map} >
-            <Canvas
-                shadows={true}
-            >
-                <Perf position="top-left" />
-                <Suspense fallback={<Instructive />}>
-                    <Lights />
-                    <Environments />
-                    <Physics debug={true}>
-                        <Level1World />
-                        <Level1WorldStairs />
-                        <ZoneSensors/>
-                        <>
-                        {displayLife &&
-                              <Ecctrl
-                                  camInitDis={-2}
-                                  camMaxDis={-2}
-                                  maxVelLimit={5}
-                                  jumpVel={4}
-                                  position={[0, 4, -5]}
-                              >
-                              <Avatar />
-                            </Ecctrl>   
-                          }                         
-                          </>
-                          <Press position={[0, -0.5, -158]} />
-                          <Rat position={[0, 1, -135]} />
-                    </Physics>
-                    <Texts />
-                </Suspense>
-                <Controls />
-            </Canvas>
-            <GameUI />
-            <NextLevelButton to="/level2" />
-        </KeyboardControls>
-    )
+    // Cambios en mostrar/ocultar elementos dependiendo del valor
+    if (lifeState.value <= 0) {
+      setDisplayLife(false);
+      console.log("Mori");
+    } else {
+      setDisplayLife(true);
+    }
+  }, [lifeState.value]); // Depende unicamente de cambios en lifeState.value    
+
+  return (
+    <KeyboardControls map={map} >
+      <Canvas
+        shadows={true}
+      >
+        <Perf position="top-left" />
+        <Suspense fallback={<Instructive />}>
+          <Lights />
+          <Environments />
+          <Physics debug={true}>
+            <Level1World />
+            <ZoneSensors />
+            <Interactables />
+            <>
+              {displayLife &&
+                <Ecctrl
+                  camInitDis={-2}
+                  camMaxDis={-2}
+                  maxVelLimit={5}
+                  jumpVel={4}
+                  position={[0, 4, -5]}
+                >
+                  <Avatar />
+                </Ecctrl>
+              }
+            </>
+            <Rat position={[0, 1, -135]} />
+          </Physics>
+          <Texts />
+        </Suspense>
+        <Controls />
+      </Canvas>
+      <GameUI />
+      <NextLevelButton to="/level2" />
+    </KeyboardControls>
+  )
 }
