11b72e8528a24401df51a375ff81e22c06236b18
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2024-05-18 17:23:34 -0500

    checkpoints and collectibles generation reimplementation

--- src/pages/level2/Level2.jsx
+++ src/pages/level2/Level2.jsx
@@ -14,7 +14,6 @@ import Environments from "./staging/Environments";
 import Level2World from "./world/Level2World";
 import Level2WorldZone1 from "./world/Level2WorldZone1";
 import Texts from "./abstractions/Texts";
-import Collectables from "./collectables/Collectables";
 import GameUI from "../../utils/components/layouts/GameUI/GameUI";
 import NextLevelButton from "../../utils/components/layouts/GameUI/components/NextLevelButton";
 import ZoneSensors from "./world/ZoneSensors";
@@ -23,22 +22,28 @@ import { useLifeState } from "../../utils/components/controller/CharacterLife";
 import GameOverScene from "../../utils/components/layouts/GameOverScene/GameOverScene";
 import SymbolicSensors from "./world/SymbolicSensors";
 import PortalNextWorld from "../../globals/interactables/PortalNextWorld";
-import Checkpoints from "./checkpoints/Checkpoints";
-import { useCheckpointStateLevel1 } from "./checkpoints/CharacterCheckpointStateLevel1";
+import { useCharacterPositionState } from "../../utils/components/controller/CharacterPositionState";
+import collectablesData from "./collectables/CollectablesData.json";
+import checkpointsData from "./checkpoints/CheckpointsData.json"
+import Collectables from "../../globals/collectables/CollectablesGenerator";
+import Checkpoints from "../../globals/interactables/CheckpointsGenerator";
 import { obtenerDeLocalStorage } from "../../utils/localStorageUtils";
+
+const debug = false;
 
 export default function Level2() {
     const map = useMovements();
+
     const lifeState = useLifeState();
     const [displayLife, setDisplayLife] = useState(true);
-    const checkpointStateLevel1 = useCheckpointStateLevel1();
+
+    const positionState = useCharacterPositionState();
+    const [actualPosition, setActualPosition] = useState(positionState.initialPosition)
 
     useEffect(() => {
         if (obtenerDeLocalStorage("actualPosition"))
             setActualPosition(obtenerDeLocalStorage("actualPosition"))
     }, obtenerDeLocalStorage("actualPosition"))
-
-    const [actualPosition, setActualPosition] = useState(checkpointStateLevel1.initialPosition)
 
     useEffect(() => {
         if (lifeState.value <= 0)
@@ -48,49 +53,51 @@ export default function Level2() {
     }, [lifeState.value]);
 
     return (
-        <KeyboardControls map={map} >
-            <Canvas
-                shadows={true}
-            >
-                <Suspense fallback={<Instructive />}>
+        <>
+            <KeyboardControls map={map} >
+                <Canvas
+                    shadows={true}
+                >
                     <Perf position="top-left" />
-                    <Lights />
-                    <Environments />
-                    <Physics debug={true}>
-                        <Level2World />
-                        <Level2WorldZone1 />
-                        <ZoneSensors />
-                        <Checkpoints />
-                        <SymbolicSensors />
-                        <Collectables />
-                        <Interactables />
-                        <PortalNextWorld
-                            position={[28, 1, -15]}
-                            nextLevel='/level3'
-                        />
-                        {displayLife &&
-                            <Ecctrl
-                                camInitDis={-2}
-                                camMaxDis={-2}
-                                maxVelLimit={4}
-                                jumpVel={3}
-                                position={actualPosition}
-                                slopeMaxAngle={Math.PI / 5.5}
-                            >
-                                <Avatar />
-                            </Ecctrl>
-                        }
-                    </Physics>
-                    <Texts />
-                </Suspense>
-                <Controls />
-            </Canvas>
-            {!displayLife &&
-                <GameOverScene
-                    reloadLevel='/level2'
-                />}
-            <GameUI />
-            <NextLevelButton to="/level3" />
-        </KeyboardControls >
+                    <Suspense fallback={<Instructive />}>
+                        <Lights />
+                        <Environments />
+                        <Physics debug={debug}>
+                            <Checkpoints checkpointsData={checkpointsData} />
+                            <Collectables collectablesData={collectablesData} />
+                            <Level2World />
+                            <Level2WorldZone1 />
+                            <ZoneSensors />
+                            <SymbolicSensors />
+                            <Interactables />
+                            <PortalNextWorld
+                                position={[28, 1, -15]}
+                                nextLevel='/level3'
+                            />
+                            {displayLife &&
+                                <Ecctrl
+                                    camInitDis={-2}
+                                    camMaxDis={-2}
+                                    maxVelLimit={4}
+                                    jumpVel={3}
+                                    position={actualPosition}
+                                    slopeMaxAngle={Math.PI / 5.5}
+                                >
+                                    <Avatar />
+                                </Ecctrl>
+                            }
+                        </Physics>
+                        <Texts />
+                    </Suspense>
+                    <Controls />
+                </Canvas>
+                {!displayLife &&
+                    <GameOverScene
+                        reloadLevel='/level2'
+                    />}
+                <GameUI />
+                <NextLevelButton to="/level3" />
+            </KeyboardControls >
+        </>
     )
 }
