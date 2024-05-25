80991662cfae779d042e506c9ffd1cf5aebc1e46
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2024-05-25 15:15:08 -0500

    Updated villain

--- src/pages/level1/Level1.jsx
+++ src/pages/level1/Level1.jsx
@@ -33,13 +33,15 @@ import NextLevelButton from '../../utils/components/layouts/GameUI/components/Ne
 import { useAuth } from '../../context/AuthContext'
 import Logout from '../../utils/components/layouts/logout/Logout'
 import { createUser, readUSer } from '../../utils/db/users-collection'
+import Villains from '../../globals/villains/VillainsGenerator'
+import VillainsData from './villains/VillainsData.json'
 
 const debug = process.env.REACT_APP_DEBUG === 'true'
 
-export default function Level1 () {
+export default function Level1() {
   const map = useMovements()
 
-  const auth = useAuth();
+  const auth = useAuth()
 
   const saveDataUser = async (valuesUser) => {
     await createUser(valuesUser)
@@ -47,19 +49,19 @@ export default function Level1 () {
 
   const readDataUser = async (email) => {
     await readUSer(email)
-    .then((result) => console.log(result))
-    .catch((error) => console.log(error))
+      .then((result) => console.log(result))
+      .catch((error) => console.log(error))
   }
 
   useEffect(() => {
     if (auth.userLogged) {
-      const { displayName, email } = auth.userLogged 
+      const { displayName, email } = auth.userLogged
 
       saveDataUser({
         displayName: displayName,
         email: email
       })
-      
+
       const result = readDataUser(email)
       console.log(result)
     }
@@ -69,41 +71,43 @@ export default function Level1 () {
   const [displayLife, setDisplayLife] = useState(true)
 
   const positionState = useCharacterPositionState()
-  const [actualPosition, setActualPosition] = useState(positionState.initialPosition)
+  const [actualPosition, setActualPosition] = useState(
+    positionState.initialPosition
+  )
 
   useEffect(() => {
-    if (obtenerDeLocalStorage('actualPosition')) { setActualPosition(obtenerDeLocalStorage('actualPosition')) }
+    if (obtenerDeLocalStorage('actualPosition')) {
+      setActualPosition(obtenerDeLocalStorage('actualPosition'))
+    }
   }, obtenerDeLocalStorage('actualPosition'))
 
   useEffect(() => {
-    if (lifeState.value <= 0) { setDisplayLife(false) } else { setDisplayLife(true) }
+    if (lifeState.value <= 0) {
+      setDisplayLife(false)
+    } else {
+      setDisplayLife(true)
+    }
   }, [lifeState.value])
 
   return (
     <>
       <KeyboardControls map={map}>
-      <Logout />
-        <Canvas
-          shadows
-        >
-          {debug &&
-            <Perf position='top-left' />}
+        <Logout />
+        <Canvas shadows>
+          {debug && <Perf position='top-left' />}
           <Suspense fallback={<Instructive />}>
             <Lights />
             <Environments />
             <Physics debug={debug}>
               <Level1World />
-              <PortalNextWorld
-                position={[0, 0, -224]}
-                nextLevel='/level2'
-              />
+              <PortalNextWorld position={[0, 0, -224]} nextLevel='/level2' />
               <Checkpoints checkpointsData={checkpointsData} />
               <Collectables collectablesData={collectablesData} />
               <Interactables />
               <SymbolicSensors />
               <ZoneSensors />
               <>
-                {displayLife &&
+                {displayLife && (
                   <Ecctrl
                     camInitDis={-2}
                     camMaxDis={-2}
@@ -112,21 +116,18 @@ export default function Level1 () {
                     position={actualPosition}
                   >
                     <Avatar />
-                  </Ecctrl>}
+                  </Ecctrl>
+                )}
               </>
               <Button position={[0, -0.5, -158]} />
-              <Rat position={[0, 0, -135]} />
+              <Villains villainsData={VillainsData} />
             </Physics>
             <Texts />
           </Suspense>
           <Controls />
         </Canvas>
-        {!displayLife &&
-          <GameOverScene
-            reloadLevel='/level1'
-          />}
-        {debug &&
-          <NextLevelButton to='/level2' />}
+        {!displayLife && <GameOverScene reloadLevel='/level1' />}
+        {debug && <NextLevelButton to='/level2' />}
         <GameUI />
       </KeyboardControls>
     </>
