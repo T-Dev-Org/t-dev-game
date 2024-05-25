0e9fcb6fd81150e07107c516944e001606fde703
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2024-05-25 14:54:44 -0500

    updated Login Functionality for Guest and Google Account Sign-In

--- src/pages/login/components/LoginComponent.jsx
+++ src/pages/login/components/LoginComponent.jsx
@@ -1,13 +1,14 @@
 import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../../../context/AuthContext'
 
-export default function LoginComponent () {
+export default function LoginComponent() {
   const navigate = useNavigate()
   const auth = useAuth()
 
   const onHandleButtonLogin = async (e) => {
     e.preventDefault()
-    await auth.loginWithGoogle()
+    await auth
+      .loginWithGoogle()
       .then((res) => {
         navigate('/level1')
       })
@@ -31,34 +32,22 @@ export default function LoginComponent () {
 
         <form className='mx-4'>
           <div className='my-4'>
-            <label className='form-label h4 my-2' htmlFor='inputEmail'>
-              Correo Electrónico
-            </label>
-            <input
-              className='form-control '
-              type='email'
-              id='inputEmail'
-              aria-describedby='emailHelp'
-            />
-            <label className='form-label h4 my-2' htmlFor='inputPassword'>
-              Contraseña
-            </label>
-            <input
-              className='form-control'
-              type='password'
-              id='inputPassword'
-            />
-            <div>
-              <button className='btn btn-outline-primary rounded-4 mt-4 mb-2'>
-                Iniciar Sesión
-              </button>
-            </div>
             <div>
               <button
                 onClick={(e) => onHandleButtonLogin(e)}
                 className='btn btn-outline-primary rounded-4 my-2'
               >
                 Iniciar con Google
+              </button>
+            </div>
+            <div>
+              <button
+                className='btn btn-outline-primary rounded-4 mt-4 mb-2'
+                onClick={() => {
+                  navigate('/level1')
+                }}
+              >
+                Iniciar como invitado
               </button>
             </div>
           </div>
