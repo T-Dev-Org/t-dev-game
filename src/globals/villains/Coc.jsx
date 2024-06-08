84d39c4c219325271aa7392e3e1aef60d9137a95
JuanLoaiza007 <juan.loaiza.santiago@correounivalle.edu.co>
2024-06-07 21:22:58 -0500

    animation and collider implementation adjustment

--- src/globals/villains/Coc.jsx
+++ src/globals/villains/Coc.jsx
@@ -16,14 +16,15 @@ export default function Coc({ onDeath, ...props }) {
   const characterBasickAttackState = useCharacterBasicAttack()
 
   const [life, setLife] = useState(10) // Vida del perro
-  const scale = 0.02
-  const positionCorrection = [0, 1.4, 0.8]
+  const scale = 2
+  const colliderScale = scale * 0.01
+  const positionCorrection = [0.45, 0, 0]
 
   const [showSparkles, setShowSparkles] = useState(false)
 
   useEffect(() => {
-    if (actions.Dance) {
-      actions.Dance.play()
+    if (actions) {
+      actions.Dancing.play()
     }
   }, [actions])
 
@@ -53,9 +54,13 @@ export default function Coc({ onDeath, ...props }) {
     <>
       {life > 0 && (
         <RigidBody type='fixed' colliders={false}>
-          <group ref={group} {...props} dispose={null}>
+          <group ref={group} {...props} scale={2} dispose={null}>
             <CuboidCollider
-              args={[34 * scale, 60 * scale, 34 * scale]}
+              args={[
+                16 * colliderScale,
+                60 * colliderScale,
+                20 * colliderScale
+              ]}
               position={positionCorrection}
               onCollisionEnter={(event) => {
                 console.log(`${event.colliderObject.name} collided`)
@@ -67,7 +72,11 @@ export default function Coc({ onDeath, ...props }) {
               }}
             />
             <CuboidCollider
-              args={[70 * scale, 70 * scale, 70 * scale]}
+              args={[
+                50 * colliderScale,
+                60 * colliderScale,
+                50 * colliderScale
+              ]}
               position={positionCorrection}
               onIntersectionEnter={(event) => handleIntersectionEnter(event)}
               onCollisionExit={(event) => handleIntersectionExit(event)}
@@ -83,45 +92,39 @@ export default function Coc({ onDeath, ...props }) {
                 speed={2}
               />
             )}
-            <group name='Scene'>
-              <group
-                name='CocArmature'
-                rotation={[Math.PI / 2, 0, 0]}
-                scale={0.01}
-              >
-                <primitive object={nodes.mixamorigHips} />
-                <group name='Coc'>
-                  <skinnedMesh
-                    name='Plane001'
-                    geometry={nodes.Plane001.geometry}
-                    material={materials['Cuerpo.001']}
-                    skeleton={nodes.Plane001.skeleton}
-                  />
-                  <skinnedMesh
-                    name='Plane001_1'
-                    geometry={nodes.Plane001_1.geometry}
-                    material={materials['Dientes.001']}
-                    skeleton={nodes.Plane001_1.skeleton}
-                  />
-                  <skinnedMesh
-                    name='Plane001_2'
-                    geometry={nodes.Plane001_2.geometry}
-                    material={materials['Negro.001']}
-                    skeleton={nodes.Plane001_2.skeleton}
-                  />
-                  <skinnedMesh
-                    name='Plane001_3'
-                    geometry={nodes.Plane001_3.geometry}
-                    material={materials['Ojo.001']}
-                    skeleton={nodes.Plane001_3.skeleton}
-                  />
-                  <skinnedMesh
-                    name='Plane001_4'
-                    geometry={nodes.Plane001_4.geometry}
-                    material={materials['Cresta.001']}
-                    skeleton={nodes.Plane001_4.skeleton}
-                  />
-                </group>
+            <group name='CocArmature' rotation={[Math.PI / 2, 0, 0]}>
+              <primitive object={nodes.mixamorigHips} />
+              <group name='Coc'>
+                <skinnedMesh
+                  name='Plane001'
+                  geometry={nodes.Plane001.geometry}
+                  material={materials['Cuerpo.001']}
+                  skeleton={nodes.Plane001.skeleton}
+                />
+                <skinnedMesh
+                  name='Plane001_1'
+                  geometry={nodes.Plane001_1.geometry}
+                  material={materials['Dientes.001']}
+                  skeleton={nodes.Plane001_1.skeleton}
+                />
+                <skinnedMesh
+                  name='Plane001_2'
+                  geometry={nodes.Plane001_2.geometry}
+                  material={materials['Negro.001']}
+                  skeleton={nodes.Plane001_2.skeleton}
+                />
+                <skinnedMesh
+                  name='Plane001_3'
+                  geometry={nodes.Plane001_3.geometry}
+                  material={materials['Ojo.001']}
+                  skeleton={nodes.Plane001_3.skeleton}
+                />
+                <skinnedMesh
+                  name='Plane001_4'
+                  geometry={nodes.Plane001_4.geometry}
+                  material={materials['Cresta.001']}
+                  skeleton={nodes.Plane001_4.skeleton}
+                />
               </group>
             </group>
           </group>
